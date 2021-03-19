031c301b0b208f4ad3220dbb8fe70afc1ce02344
Giovix92 <ggualtierone@gmail.com>
2021-03-19 15:47:55 +0000

    msm8998-common: Adapt to RevengeOS

--- common.mk
+++ common.mk
@@ -28,11 +28,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-dot
+    $(LOCAL_PATH)/overlay-revengeos
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-dot/packages/apps/Snap \
+    $(LOCAL_PATH)/overlay-revengeos/packages/apps/Snap \
 
 # Permissions
 PRODUCT_COPY_FILES += \
