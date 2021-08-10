AOSiP-Devices/device_oneplus_sm8150-common

msm8998-common: DeviceSettings: Send broadcast on alert slider change
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
Change-Id: I23f45892be9860c7c9876852100c1baac9eae112

DeviceSettings/src/com/aosip/device/DeviceSettings/Constants.java
@@ -69,6 +69,10 @@ public class Constants {
     public static final Map<Integer, String> sKeyMap = new HashMap<>();
     public static final Map<String, Integer> sKeyDefaultMap = new HashMap<>();
 
+    public static final String ACTION_UPDATE_SLIDER_POSITION
+            = "com.aosip.device.DeviceSettings.UPDATE_SLIDER_POSITION";
+    public static final String EXTRA_SLIDER_POSITION = "position";
+
     // Broadcast extra: keycode mapping (int[]: key = gesture ID, value = keycode)
     static final String UPDATE_EXTRA_KEYCODE_MAPPING = "keycode_mappings";
     // Broadcast extra: assigned actions (int[]: key = gesture ID, value = action)

karthik990/platform_device_oneplus_msm8998-common

DeviceSettings/src/com/derp/device/DeviceSettings/Constants.java
@@ -69,6 +69,10 @@ public class Constants {
     public static final Map<Integer, String> sKeyMap = new HashMap<>();
     public static final Map<String, Integer> sKeyDefaultMap = new HashMap<>();
 
+    public static final String ACTION_UPDATE_SLIDER_POSITION
+            = "com.derp.device.DeviceSettings.UPDATE_SLIDER_POSITION";
+    public static final String EXTRA_SLIDER_POSITION = "position";
+
     // Broadcast extra: keycode mapping (int[]: key = gesture ID, value = keycode)
     static final String UPDATE_EXTRA_KEYCODE_MAPPING = "keycode_mappings";
     // Broadcast extra: assigned actions (int[]: key = gesture ID, value = action)